b9d8d96b4c5ac4159fd3442812887122464949cb
Yasasiru <siriwardhanaglaybs.21@uom.lk>
2024-05-21 21:21:17 +0530

    Update simple greeting programme.py

--- simple greeting programme.py
+++ simple greeting programme.py
@@ -1,20 +1,2 @@
-# version 1
 def greet():
     print("Hello!")
-
-# version 2
-def greet(name):
-    print(f"Hello, {name}!")
-
-# version 3
-def greet(name="World"):
-    print(f"Hello, {name}!")
-
-# version 4
-def greet(name="World", language="English"):
-    if language == "English":
-        print(f"Hello, {name}!")
-    elif language == "Spanish":
-        print(f"Hola, {name}!")
-    else:
-        print("Invalid language.")
